79595206c3c0863134a7ce6dee4fc3f21ff2f0b5
OTWhite04 <owen.t.white2005@gmail.com>
2025-01-27 15:05:22 -0400

    Learning Scene Manager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+
+    public GameObject sprite;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(0);
+            GameObject.Find("sprite");
+        }
+
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene(1);
+            GameObject.Find("sprite");
+        }
+    
     }
 }
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+
+    static Singleton Instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Instance != null)
+        {
+            GameObject.Destroy(gameObject);
+        }
+        else
+        {
+            GameObject.DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
